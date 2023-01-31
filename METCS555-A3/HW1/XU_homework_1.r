667dcd2fcfca622a43f848fbcb4ea543b1b8e286
xyh0927 <xyhmgbk@163.com>
2023-01-31 14:49:45 -0500

    555hw1 final release update

--- METCS555-A3/HW1/XU_homework_1.r
+++ METCS555-A3/HW1/XU_homework_1.r
@@ -5,7 +5,8 @@ data <- unlist(data, use.names = FALSE)
 print(data)
 
 #q2-3
-hist(data,xlim = c(0,17), breaks = seq(0, 15, 1), ylim = c(0, 25))
+histogram <- hist(data,xlim = c(0,17), breaks = seq(0, 15, 1), ylim = c(0, 25))
+print(histogram)
 summary_q2 <- summary(data)
 print(summary_q2)
 IQR <- 7.00 - 4.00
@@ -14,9 +15,13 @@ MIN_Outliers <- 4 - 1.5 * IQR
 MAX_Outliers <- 7 + 1.5 * IQR
 print(MIN_Outliers)
 print(MAX_Outliers)
+table_data <- table(data)
+print(table_data)
+sd <- sd(data)
+print(sd)
 
 #q4
-Percent_Patient_less_10 <- pnorm(10, 6, 3)
-p_greater_6 <- 1 - pnorm(6, 5, 3/sqrt(35))
+Percent_Patient_less_10 <- pnorm(10, 5, 3)
 print(Percent_Patient_less_10)
+p_greater_6 <- 1 - pnorm(6, 5, 3/sqrt(35))
 print(p_greater_6)
